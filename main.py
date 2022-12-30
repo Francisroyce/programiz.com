bb45af65667ac9a994d72caa9b8653859b1d92f6
Francisroyce <francisuiroyceux@gmail.com>
2022-12-30 14:47:21 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# full lesson from programiz.com
+
+# day one assigning values to variables
+
+site_name = 'programiz.com'
+print(site_name)
+
+site_name ='apple.com'
+print(site_name)
+
+#assigning multiple values to multiple vareiables
+a, b, c, =5,3.2, 'hello'
+print(a)
+print(b)
+print(c)
+
+#same value to multiple variable
+
+a = b = 'francis Royce'
+print(a)
+print(b)
+
+#case sensitivity
+num = 6
+Num = 44
+print(num)
+print(Num)
+
+#python constants/creating constant
+
+#special literals
+value = None
+print(value)
+
+#4 litetral collections:
+# 1- list Literal
+fruits = ['apple', 'mango', 'orange']
+print(fruits)
+
+# 2 tuple literal
+numbers = (1, 2, 3)
+print(numbers)
+
+# 3 dictionary literal
+alphabets = {'a':  'apple', 'b': 'ball', 'c': 'cat'}
+print(alphabets)
+
+# 4 set literals
+vowels = {'a', 'e', 'i', 'o', 'u'}
+print(vowels)
+
